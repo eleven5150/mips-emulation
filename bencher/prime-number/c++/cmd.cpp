2f5272cbc0f5c075ea0796065b2881639d1d7b23
Kirill <kirSM2010@gmail.com>
2022-10-16 18:37:25 +0300

    Added source prime number test

---
+++ bencher/prime-number/c++/cmd.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int32_t primeNumberCount = 100;
+    if (argc == 2) {
+        primeNumberCount = atoi(argv[1]);
+    }
+
+    int32_t number = 0;
+    while (primeNumberCount > 0) {
+        number++;
+
+        int32_t j = 0;
+        for (int32_t i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                j++;
+            }
+        }
+
+        if (j == 2) {
+            primeNumberCount--;
+        }
+    }
+
+    printf("The latest prime number: %d\n", number);
+}
