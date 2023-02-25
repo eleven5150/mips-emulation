3adc049764a22de5a6d0658ff9b98bd7ea3d21d8
Kirill <kirSM2010@gmail.com>
2023-02-25 18:37:37 +0300

    Another update of C# hello world

---
+++ bencher/tests/c#/hello_world/test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hello
+{
+    class Program
+    {
+        public static void Main (string[] args)
+        {
+            Console.WriteLine ("Hello World!\n");
+        }
+    }
+}
