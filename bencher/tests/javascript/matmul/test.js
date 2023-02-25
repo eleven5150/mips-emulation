3b1c3fd148d2644d541ebd638fb2f5245fbd7ea2
Kirill <kirSM2010@gmail.com>
2023-02-26 01:03:20 +0300

    Added matmul on js

--- bencher/tests/javascript/matmul/test.js
+++ bencher/tests/javascript/matmul/test.js
@@ -1,41 +1,94 @@
+'use strict';
 
-function multiply(mat1, mat2, res)
-{
-    let i, j, k;
-    let N = mat1.length
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < N; j++) {
-            res[i][j] = 0;
-            for (k = 0; k < N; k++)
-            res[i][j] += mat1[i][k] * mat2[k][j]
+const process = require('process');
+const fs = require('fs');
+
+const MATRIX_DIMENSION = 1000;
+
+const Matrix = {
+    init: function (matrix_dimension) {
+        const matrix = new Array(matrix_dimension);
+        for (let i = 0; i < matrix_dimension; i++)
+            matrix[i] = new Uint32Array(matrix_dimension);
+        return matrix;
+    },
+    transpose: function (matrix, matrix_dimension) {
+        const t_matrix = Matrix.init(matrix_dimension);
+        for (let i = 0; i < matrix_dimension; i++) {
+            for (let j = 0; j < matrix_dimension; j++) {
+                t_matrix[i][j] = matrix[j][i];
+            }
         }
+        return t_matrix;
+    },
+    multiply: function (matrix_a, matrix_b, matrix_dimension) {
+        const matrix_result = Matrix.init(matrix_dimension);
+        const t_matrix_b = Matrix.transpose(matrix_b, matrix_dimension);
+        for (let i = 0; i < matrix_dimension; i++) {
+            for (let j = 0; j < matrix_dimension; j++) {
+                let sum = 0;
+                for (let k = 0; k < matrix_dimension; k++)
+                    sum = sum + matrix_a[i][k] * t_matrix_b[j][k];
+                matrix_result[i][j] = sum;
+            }
+        }
+        return matrix_result;
+    },
+    print: function (matrix, matrix_dimension) {
+        for (let i = 0; i < matrix_dimension; i++) {
+            for (let j = 0; j < matrix_dimension; j++) {
+                process.stdout.write(`${matrix[i][j]}\t`);
+            }
+            console.log("\n")
+        }
+        console.log("\n")
     }
+};
+
+function create_matrix(file_path) {
+    let matrix = Matrix.init(MATRIX_DIMENSION);
+    let data = fs.readFileSync(file_path, "utf8");
+    if (!data) {
+        console.error(`File ${file_path} opening failed`);
+        return 0;
+    }
+    let i = 0;
+    let j = 0;
+    let strings = data.split(/\n/);
+    strings.pop()
+    for (const line_num in strings) {
+        let records = strings[line_num].split(/,/);
+        for (const record_num in records) {
+            matrix[i][j] = parseInt(records[record_num], 10);
+            j++;
+        }
+        i++;
+        j = 0;
+    }
+
+    // Matrix.print(matrix, MATRIX_DIMENSION)
+    return matrix;
 }
 
-let fs = require('fs');
-let all = fs.readFileSync('tests/datasets/matmul/dataset.txt', "utf8");
-all = all.trim();  // final crlf in file
-let lines = all.split("\n");
-let n = lines.length;
-var mas = [];
-	for (var i = 0; i < n; i++){
-	    mas[i] = [];
-	    for (var j = 0; j < n; j++){
-	        mas[i][j] = 0;
-	}}
+function main(argc, argv) {
+    if (argc < 2) {
+        console.error("Error! Both paths to matrix data required\n");
+        return 1;
+    }
 
-for (let i = 0; i < n; ++i) {  // each line
-  let tokens = lines[i].split(" ");
-  for (let j = 0; j < n; ++j) {  // each val curr line
-    mas[i][j] = parseInt(tokens[j]);
-  }
+    let matrix_a_file = argv[2];
+    let matrix_b_file = argv[3];
+
+    let matrix_a = create_matrix(matrix_a_file);
+    let matrix_b = create_matrix(matrix_b_file);
+
+    if (!matrix_a || !matrix_b) {
+        return 1;
+    }
+
+    let matrix_result = Matrix.multiply(matrix_a, matrix_b, MATRIX_DIMENSION);
+
+    // Matrix.print(matrix_result, MATRIX_DIMENSION)
 }
 
-var res = [];
-	for (var i = 0; i < n; i++){
-	    res[i] = [];
-	    for (var j = 0; j < n; j++){
-	        res[i][j] = 0;
-	}}
-
-multiply(mas,mas,res)
+main(process.argv.length, process.argv);
