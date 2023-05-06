4684fc726a8c8dd9ea4670bc7620cd2cbce0ab0d
Kirill <kirSM2010@gmail.com>
2023-05-07 01:19:13 +0300

    Added cython matmul test

--- bencher/tests/cython/matmul/test.pyx
+++ bencher/tests/cython/matmul/test.pyx
@@ -1,25 +1,112 @@
 cimport libc.stdio as c
+from libc.stdlib cimport malloc, calloc, free, atoi
+from libc.string cimport strtok
 
-def matrix_multiply(u, v, res):
-    for i in range(1000):
-        for j in range(1000):
-            for k in range(1000):
-                res[i][j] += u[i][k] * v[k][j]
-    return res
+import sys
 
-    
-cdef c.FILE *file_ptr = c.fopen("tests/datasets/matmul/dataset.txt", "r")
-cdef size_t i = 0
-cdef size_t j = 0
-cdef size_t size = 1000
-cdef int array[1000][1000] 
-cdef int res[1000][1000]
+MATRIX_DIMENSION = 1000
 
-while(i < size):
-    while(j < size):
-        c.fscanf(file_ptr, "%d", &array[i][j])
-        j+=1
-    j = 0
-    i+=1
-c.fclose(file_ptr)
-matrix_multiply(array,array,res)
+
+cdef void matrix_print(unsigned int **matrix, int matrix_dimension):
+    for i in range(0, matrix_dimension):
+        for j in range(0, matrix_dimension):
+            c.printf("%u\t", matrix[i][j])
+        c.printf("\n")
+    c.printf("\n")
+
+
+cdef unsigned int **matrix_init(unsigned int matrix_dimension):
+    cdef unsigned int **matrix = <unsigned int **> malloc(matrix_dimension * sizeof(unsigned int *))
+    for i in range(0, matrix_dimension):
+        matrix[i] = <unsigned int *> calloc(matrix_dimension, sizeof(unsigned int))
+    return matrix
+
+
+cdef void matrix_free(int matrix_dimension, unsigned int **matrix):
+    for i in range(0, matrix_dimension):
+        free(matrix[i])
+    free(matrix)
+
+
+cdef unsigned int **create_matrix(char *file_name):
+    cdef unsigned int **matrix = matrix_init(<unsigned int> MATRIX_DIMENSION)
+    cdef c.FILE *fstream = c.fopen(file_name, "r")
+    if fstream == NULL:
+        c.printf("file %s opening failed\n", file_name)
+        exit(1)
+
+    c.fseek(fstream, 0, c.SEEK_END)
+    cdef unsigned int file_size = c.ftell(fstream)
+    c.fseek(fstream, 0, c.SEEK_SET)
+
+    cdef char *buffer = <char *> malloc(file_size * sizeof(char))
+    cdef char *record
+    cdef int i = 0
+    cdef int j = 0
+    while c.fgets(buffer, file_size, fstream) != NULL:
+        record = strtok(buffer, ",")
+        while record != NULL:
+            matrix[i][j] = <unsigned int> atoi(record)
+            record = strtok(NULL, ",")
+            j += 1
+        i += 1
+        j = 0
+
+    matrix_print(matrix, <int> MATRIX_DIMENSION)
+    return matrix
+
+
+cdef unsigned int **matrix_multiply(int matrix_dimension, unsigned int **matrix_a, unsigned int **matrix_b):
+    cdef unsigned int **matrix_result
+    cdef unsigned int **t_matrix_b
+    matrix_result = matrix_init(matrix_dimension)
+    t_matrix_b = matrix_init(matrix_dimension)
+    for i in range(0, matrix_dimension):
+        for j in range(0, matrix_dimension):
+            t_matrix_b[i][j] = matrix_b[j][i]
+
+    cdef unsigned int *p
+    cdef unsigned int *q
+    cdef unsigned int *r
+    cdef unsigned int t
+    for i in range(0, matrix_dimension):
+        p = matrix_a[i]
+        q = matrix_result[i]
+        for j in range(0, matrix_dimension):
+            t = 0
+            r = t_matrix_b[j]
+            for k in range(0, matrix_dimension):
+                t += p[k] * r[k]
+            q[j] = t
+
+    matrix_free(matrix_dimension, t_matrix_b)
+    return matrix_result
+
+
+def main(raw_args):
+    if len(raw_args) < 2:
+        print("Error! Both paths to matrix data required\n")
+        exit(1)
+
+    matrix_a_path: str = raw_args[1]
+    matrix_b_path: str = raw_args[2]
+
+    matrix_a_path_encoded = matrix_a_path.encode("ascii")
+    cdef char *matrix_a_file = <char *>matrix_a_path_encoded
+
+    matrix_b_path_encoded = matrix_b_path.encode("ascii")
+    cdef char *matrix_b_file = <char *>matrix_b_path_encoded
+
+    cdef unsigned int **matrix_a = create_matrix(matrix_a_file)
+    cdef unsigned int **matrix_b = create_matrix(matrix_b_file)
+
+    cdef unsigned int **matrix_result = matrix_multiply(<int> MATRIX_DIMENSION, matrix_a, matrix_b)
+
+    matrix_print(matrix_result, MATRIX_DIMENSION)
+
+    matrix_free(MATRIX_DIMENSION, matrix_a)
+    matrix_free(MATRIX_DIMENSION, matrix_b)
+    matrix_free(MATRIX_DIMENSION, matrix_result)
+
+
+main(sys.argv)
