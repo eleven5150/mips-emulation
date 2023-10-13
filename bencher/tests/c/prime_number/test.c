f5e0dec3c08658705bee6ccf7ae057e8758ddbf7
Kirill <kirSM2010@gmail.com>
2023-10-14 01:00:48 +0300

    Fixed prime number in c

--- bencher/tests/c/prime_number/test.c
+++ bencher/tests/c/prime_number/test.c
@@ -1,28 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-    int32_t primeNumberCount = 100;
-    if (argc == 2) {
-        primeNumberCount = atoi(argv[1]);
+    if (argc < 2) {
+        printf("Error! File with prime curr_number count must be specified\n");
+        exit(EXIT_FAILURE);
     }
 
-    int32_t number = 0;
-    while (primeNumberCount > 0) {
-        number++;
+    char *file_path = argv[1];
+    FILE *fstream = fopen(file_path, "r");
+    if (fstream == NULL) {
+        printf("file %s opening failed\n", file_path);
+        exit(EXIT_FAILURE);
+    }
 
-        int32_t j = 0;
-        for (int32_t i = 1; i <= number; i++) {
-            if (number % i == 0) {
+    fseek(fstream, 0, SEEK_END);
+    int file_size = ftell(fstream);
+    fseek(fstream, 0, SEEK_SET);
+
+    char *buffer = (char *) malloc(file_size * sizeof(char));
+    fgets(buffer, file_size, fstream);
+
+    fclose(fstream);
+
+    unsigned int prime_number_count = (unsigned int)atoi(buffer);
+    free(buffer);
+
+    unsigned int curr_number = 0;
+    while (prime_number_count > 0) {
+        curr_number++;
+
+        unsigned int j = 0;
+        for (unsigned int i = 1; i <= curr_number; i++) {
+            if (curr_number % i == 0) {
                 j++;
             }
         }
 
         if (j == 2) {
-            primeNumberCount--;
+            prime_number_count--;
         }
     }
 
-    printf("The latest prime number: %d\n", number);
+    printf("The latest prime number: %d\n", curr_number);
+    return 0;
 }
