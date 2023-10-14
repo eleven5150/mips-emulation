0a7fa1a189669d46dc5297d8825486db43f34e81
Kirill <kirSM2010@gmail.com>
2023-10-14 19:30:20 +0300

    Little fixes in commands

--- bencher/bencher.py
+++ bencher/bencher.py
@@ -19,7 +19,7 @@ from tests import TestsConfigData, get_tests_config_data
 
 TESTS_CONFIG: str = "tests/tests-config.json"
 DATA_DIR: Path = Path("data")
-DOCKER_COMMAND: str = "docker exec bencher"
+DOCKER_COMMAND: str = "docker exec -it bencher"
 
 
 @dataclass
