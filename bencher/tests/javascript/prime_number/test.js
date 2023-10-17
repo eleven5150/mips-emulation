da9d23f8007095f45c387674b8ac4789f7c76025
Kirill <kirSM2010@gmail.com>
2023-10-17 13:28:46 +0300

    Fixed prime number in js

--- bencher/tests/javascript/prime_number/test.js
+++ bencher/tests/javascript/prime_number/test.js
@@ -1,19 +1,41 @@
-let primeNumberCount = process.argv[2] || 100;
-let number = 0;
+'use strict';
 
-while (primeNumberCount > 0) {
-    number++;
-    let j = 0;
+const process = require('process');
+const fs = require('fs');
 
-    for (let i = 1; i <= number; i++) {
-        if (number % i === 0) {
-            j++;
+function main(argc, argv) {
+    if (argc < 1) {
+        console.error("Error! File with prime number count must be specified\n");
+        return 1;
+    }
+
+    let prime_number_count_file = argv[2];
+
+    let data = fs.readFileSync(prime_number_count_file, "utf8");
+    if (!data) {
+        console.error(`File ${prime_number_count_file} opening failed`);
+        return 0;
+    }
+
+    let prime_number_count = parseInt(data);
+
+    let curr_number = 0;
+    while (prime_number_count > 0) {
+        curr_number++;
+        let j = 0;
+
+        for (let i = 1; i <= curr_number; i++) {
+            if (curr_number % i === 0) {
+                j++;
+            }
+        }
+
+        if (j === 2) {
+            prime_number_count--;
         }
     }
 
-    if (j === 2) {
-        primeNumberCount--;
-    }
+    console.log(`The latest prime number: ${curr_number}`);
 }
 
-console.log('The latest prime number: ' + number);
+main(process.argv.length, process.argv);
