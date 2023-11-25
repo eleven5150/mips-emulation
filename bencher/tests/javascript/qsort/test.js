fb121d5b2cfcf1333912dc580e1870830627c4db
Kirill <kirSM2010@gmail.com>
2023-11-25 23:02:24 +0300

    Added qsort in js

--- bencher/tests/javascript/qsort/test.js
+++ bencher/tests/javascript/qsort/test.js
@@ -1,48 +1,73 @@
-
-
-function swap(items, leftIndex, rightIndex){
-  var temp = items[leftIndex];
-  items[leftIndex] = items[rightIndex];
-  items[rightIndex] = temp;
-}
-function partition(items, left, right) {
-  var pivot   = items[Math.floor((right + left) / 2)], //middle element
-      i       = left, //left pointer
-      j       = right; //right pointer
-  while (i <= j) {
-      while (items[i] < pivot) {
-          i++;
-      }
-      while (items[j] > pivot) {
-          j--;
-      }
-      if (i <= j) {
-          swap(items, i, j); //sawpping two elements
-          i++;
-          j--;
-      }
-  }
-  return i;
-}
-
-function quickSort(items, left, right) {
-  var index;
-  if (items.length > 1) {
-      index = partition(items, left, right); //index returned from partition
-      if (left < index - 1) { //more elements on the left side of the pivot
-          quickSort(items, left, index - 1);
-      }
-      if (index < right) { //more elements on the right side of the pivot
-          quickSort(items, index, right);
-      }
-  }
-  return items;
-}
-
+const process = require('process');
 const fs = require('fs');
-fs.readFile("tests/datasets/qsort/dataset.txt",(err,data)=>{
-  if(err) throw err;
-   var array = data.toString().split(' ');
 
-  console.log(quickSort(array, 0, array.length-1));
-});
+let DATA_TO_SORT_SIZE = 0;
+
+function data_print(data, size) {
+    for (let i = 0; i < size; i++) {
+        console.log(`${data[i]} `);
+    }
+}
+
+function partition(data, low, high) {
+    let temp;
+    let pivot = data[high];
+    let i = low - 1;
+    for (let j = low; j <= high - 1; j++) {
+        if (data[j] <= pivot) {
+            i++;
+            temp = data[i];
+            data[i] = data[j];
+            data[j] = temp;
+        }
+    }
+
+    temp = data[i + 1];
+    data[i + 1] = data[high];
+    data[high] = temp;
+
+    return i + 1;
+}
+
+function quick_sort(items, low, high) {
+    if (low < high) {
+        let pi = partition(items, low, high);
+
+        quick_sort(items, low, pi - 1);
+        quick_sort(items, pi, high);
+    }
+    return items;
+}
+
+function main(argc, argv) {
+    if (argc < 1) {
+        console.error("Error! File with data to sort must be specified\n");
+        return 1;
+    }
+
+    let data_to_sort_path = argv[2];
+
+    let data = fs.readFileSync(data_to_sort_path, "utf8");
+    if (!data) {
+        console.error(`File ${data_to_sort_path} opening failed`);
+        return 0;
+    }
+
+    let strings = data.split(/\n/);
+    strings.pop();
+    let data_to_sort_size = strings[0];
+    DATA_TO_SORT_SIZE = data_to_sort_size.slice(2);
+    strings = strings.slice(1);
+
+    let data_to_sort = new BigUint64Array(DATA_TO_SORT_SIZE);
+    for (let line_num in strings) {
+        data_to_sort[line_num] = BigInt(parseInt(strings[line_num], 10));
+    }
+
+    quick_sort(data_to_sort, 0, DATA_TO_SORT_SIZE - 1);
+
+    // data_print(data_to_sort, DATA_TO_SORT_SIZE);
+
+}
+
+main(process.argv.length, process.argv);
