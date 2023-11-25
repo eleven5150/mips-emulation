7f79dc56e95c2fe65bed06136acf9bc1744c21d1
Kirill <kirSM2010@gmail.com>
2023-11-25 22:19:18 +0300

    Fixed types in js matmul

--- bencher/tests/javascript/matmul/test.js
+++ bencher/tests/javascript/matmul/test.js
@@ -3,17 +3,17 @@
 const process = require('process');
 const fs = require('fs');
 
-const MATRIX_DIMENSION = 1000;
+let MATRIX_DIMENSION = 0;
 
 const Matrix = {
     init: function (matrix_dimension) {
         const matrix = new Array(matrix_dimension);
         for (let i = 0; i < matrix_dimension; i++)
-            matrix[i] = new Uint32Array(matrix_dimension);
+            matrix[i] = new BigUint64Array(matrix_dimension);
         return matrix;
     },
     transpose: function (matrix, matrix_dimension) {
-        const t_matrix = Matrix.init(matrix_dimension);
+        let t_matrix = Matrix.init(matrix_dimension);
         for (let i = 0; i < matrix_dimension; i++) {
             for (let j = 0; j < matrix_dimension; j++) {
                 t_matrix[i][j] = matrix[j][i];
@@ -22,13 +22,19 @@ const Matrix = {
         return t_matrix;
     },
     multiply: function (matrix_a, matrix_b, matrix_dimension) {
-        const matrix_result = Matrix.init(matrix_dimension);
+        let matrix_result = Matrix.init(matrix_dimension);
         const t_matrix_b = Matrix.transpose(matrix_b, matrix_dimension);
         for (let i = 0; i < matrix_dimension; i++) {
             for (let j = 0; j < matrix_dimension; j++) {
-                let sum = 0;
-                for (let k = 0; k < matrix_dimension; k++)
+                let sum = BigInt(0);
+                for (let k = 0; k < matrix_dimension; k++) {
+                    // console.log(sum)
+                    // console.log(matrix_a[i][k])
+                    // console.log(t_matrix_b[j][k])
                     sum = sum + matrix_a[i][k] * t_matrix_b[j][k];
+                    // console.log(sum)
+                    // console.log("")
+                }
                 matrix_result[i][j] = sum;
             }
         }
@@ -39,14 +45,14 @@ const Matrix = {
             for (let j = 0; j < matrix_dimension; j++) {
                 process.stdout.write(`${matrix[i][j]}\t`);
             }
-            console.log("\n")
+            console.log("")
         }
         console.log("\n")
     }
 };
 
 function create_matrix(file_path) {
-    let matrix = Matrix.init(MATRIX_DIMENSION);
+
     let data = fs.readFileSync(file_path, "utf8");
     if (!data) {
         console.error(`File ${file_path} opening failed`);
@@ -55,11 +61,15 @@ function create_matrix(file_path) {
     let i = 0;
     let j = 0;
     let strings = data.split(/\n/);
-    strings.pop()
+    strings.pop();
+    let matrix_dimension_string = strings[0];
+    MATRIX_DIMENSION = matrix_dimension_string.slice(2);
+    let matrix = Matrix.init(MATRIX_DIMENSION);
+    strings = strings.slice(1)
     for (const line_num in strings) {
         let records = strings[line_num].split(/,/);
         for (const record_num in records) {
-            matrix[i][j] = parseInt(records[record_num], 10);
+            matrix[i][j] = BigInt(parseInt(records[record_num], 10));
             j++;
         }
         i++;
