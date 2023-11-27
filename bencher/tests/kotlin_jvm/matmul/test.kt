9363b4ad65bb568ea1f40a987fc66307e2df7271
Kirill <kirSM2010@gmail.com>
2023-11-28 01:13:08 +0300

    Fixed kotlin jvm matmul

--- bencher/tests/kotlin_jvm/matmul/test.kt
+++ bencher/tests/kotlin_jvm/matmul/test.kt
@@ -2,14 +2,14 @@ import kotlin.system.exitProcess
 import java.io.File
 
 object Constants {
-    const val MATRIX_DIMENSION: Int = 5
+    var MATRIX_DIMENSION: Int = 0;
 }
 
 object Matrix {
 
-    fun print(matrix: Array<UIntArray>, matrix_deminsion: Int) {
-        for (i in 0..matrix_deminsion - 1) {
-            for (j in 0..matrix_deminsion - 1) {
+    fun print(matrix: Array<ULongArray>, matrixDimension: Int) {
+        for (i in 0..matrixDimension - 1) {
+            for (j in 0..matrixDimension - 1) {
                 print("${matrix[i][j]}\t");
             }
             print("\n");
@@ -17,66 +17,68 @@ object Matrix {
         print("\n");
     }
 
-    fun matrix_multiply(matrix_deminsion: Int, matrix_a: Array<UIntArray>, matrix_b: Array<UIntArray>): Array<UIntArray> {
+    fun matrixMultiply(matrixDimension: Int, matrixA: Array<ULongArray>, matrixB: Array<ULongArray>): Array<ULongArray> {
 
-        val matrix_result: Array<UIntArray> = Array(Constants.MATRIX_DIMENSION) { UIntArray(Constants.MATRIX_DIMENSION) };
-        val t_matrix_b: Array<UIntArray> = Array(Constants.MATRIX_DIMENSION) { UIntArray(Constants.MATRIX_DIMENSION) };
-        for (i in 0..matrix_deminsion - 1) {
-            for (j in 0..matrix_deminsion - 1) {
-                t_matrix_b[i][j] = matrix_b[j][i];
+        val matrixResult: Array<ULongArray> = Array(Constants.MATRIX_DIMENSION) { ULongArray(Constants.MATRIX_DIMENSION) };
+        val tMatrixB: Array<ULongArray> = Array(Constants.MATRIX_DIMENSION) { ULongArray(Constants.MATRIX_DIMENSION) };
+        for (i in 0..matrixDimension - 1) {
+            for (j in 0..matrixDimension - 1) {
+                tMatrixB[i][j] = matrixB[j][i];
             }
         }
 
-        for (i in 0..matrix_deminsion - 1) {
-            for (j in 0..matrix_deminsion - 1) {
-               var s: UInt = 0u;
-                for (k in 0..matrix_deminsion - 1) {
-                    s += matrix_a[i][k] * t_matrix_b[j][k];
+        for (i in 0..matrixDimension - 1) {
+            for (j in 0..matrixDimension - 1) {
+               var s: ULong = 0u;
+                for (k in 0..matrixDimension - 1) {
+                    s += matrixA[i][k] * tMatrixB[j][k];
                 }
-                matrix_result[i][j] = s;
+                matrixResult[i][j] = s;
             }
         }
-        return matrix_result;
+        return matrixResult;
     }
 }
 
 
-fun create_matrix(file_path: String): Array<UIntArray> {
-    val matrix: Array<UIntArray> = Array(Constants.MATRIX_DIMENSION) { UIntArray(Constants.MATRIX_DIMENSION) };
+fun createMatrix(filePath: String): Array<ULongArray> {
 
-    val data: String = File(file_path).inputStream().readBytes().toString(Charsets.UTF_8)
+
+    val data: String = File(filePath).inputStream().readBytes().toString(Charsets.UTF_8)
     val lines: MutableList<String> = data.split("\n").toMutableList();
 
     lines.removeLast();
 
-    var i: Int = 0;
-    var j: Int = 0;
-    for (line in lines) {
-        val line_record: List<String> = line.split(",");
-        for (record in line_record) {
-            matrix[i][j] = record.toUInt();
-            j++;
+    val matrixDimensionString = lines[0];
+    Constants.MATRIX_DIMENSION = matrixDimensionString.substring(2).toInt()
+
+    lines.removeFirst()
+
+    val matrix: Array<ULongArray> = Array(Constants.MATRIX_DIMENSION) { ULongArray(Constants.MATRIX_DIMENSION) };
+
+    for ((i, line) in lines.withIndex()) {
+        val lineRecord: List<String> = line.split(",");
+        for ((j, record) in lineRecord.withIndex()) {
+            matrix[i][j] = record.toULong();
         }
-        i++;
-        j = 0;
     }
 
-//    Matrix.print(matrix, Constants.MATRIX_DIMENSION);
+    Matrix.print(matrix, Constants.MATRIX_DIMENSION);
     return matrix;
 }
 
-fun main(args: Array<String>) {
+fun main(args: Array<String>){
     if (args.size < 2) {
         println("Error! Both paths to matrix data required\\n")
         exitProcess(1)
     }
-    val matrix_a_path: String = args[0];
-    val matrix_b_path: String = args[1];
+    val matrixAPath: String = args[0];
+    val matrixBPath: String = args[1];
 
-    val matrix_a: Array<UIntArray> = create_matrix(matrix_a_path);
-    val matrix_b: Array<UIntArray> = create_matrix(matrix_b_path);
+    val matrixA: Array<ULongArray> = createMatrix(matrixAPath);
+    val matrixB: Array<ULongArray> = createMatrix(matrixBPath);
 
-    val matrix_result: Array<UIntArray> = Matrix.matrix_multiply(Constants.MATRIX_DIMENSION, matrix_a, matrix_b);
+    val matrixResult: Array<ULongArray> = Matrix.matrixMultiply(Constants.MATRIX_DIMENSION, matrixA, matrixB);
 
-//    Matrix.print(matrix_result, Constants.MATRIX_DIMENSION);
+    Matrix.print(matrixResult, Constants.MATRIX_DIMENSION);
 }
