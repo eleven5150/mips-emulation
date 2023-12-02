5a30ccc9a8e5ee1b985363b99fdb975e32bfc906
Kirill <kirSM2010@gmail.com>
2023-12-02 17:27:01 +0300

    Added rust prime_number

--- bencher/tests/rust/prime_number/test.rs
+++ bencher/tests/rust/prime_number/test.rs
@@ -1,20 +1,33 @@
 use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::process::exit;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let mut prime_number_count: i32 = 100;
-    if args.len() == 2 {
-        prime_number_count = args[1].parse::<i32>().unwrap();
+    if args.len() < 1 {
+        println!("Error! ile with prime number count must be specified\n");
+        exit(1);
     }
 
-    let mut number: i32 = 0;
-    while prime_number_count > 0 {
-        number += 1;
-        let mut j: i32 = 0;
+    let file_path: &String = &args[1];
 
-        for i in 1..number + 1 {
-            if number % i == 0 {
+    let file: File = File::open(file_path).unwrap();
+    let mut reader: BufReader<File> = BufReader::new(file);
+
+    let mut first_line: String = String::new();
+    let len: usize = reader.read_line(&mut first_line).unwrap();
+
+    let mut prime_number_count: u64 = first_line.trim().parse::<u64>().unwrap();
+
+    let mut curr_number: u64 = 0;
+    while prime_number_count > 0 {
+        curr_number += 1;
+
+        let mut j: u64 = 0;
+        for i in 1..curr_number + 1 {
+            if curr_number % i == 0 {
                 j += 1;
             }
         }
@@ -24,5 +37,5 @@ fn main() {
         }
     }
 
-    println!("The latest prime number: {}", number);
+    println!("The latest prime number: {}", curr_number);
 }
