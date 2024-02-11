9a40e00dfad4d660f946cc07276a50230d526674
Kirill <kirSM2010@gmail.com>
2024-02-11 15:50:34 +0300

    Added brackets

--- benchmarking/scripts/clean_all_docker.sh
+++ benchmarking/scripts/clean_all_docker.sh
@@ -3,8 +3,8 @@
 main() {
     [ "$UID" -eq 0 ] || exec sudo bash "$0"
 
-    docker stop $(docker ps -a -q)
-    docker rm $(docker ps -a -q)
+    docker stop "$(docker ps -a -q)"
+    docker rm "$(docker ps -a -q)"
 }
 
 main
