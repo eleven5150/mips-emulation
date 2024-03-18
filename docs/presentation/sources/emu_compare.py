6213caa76fa15007acfd3bdf5b92010d1f0b9a65
Kutaev Kirill <kirSM2010@gmail.com>
2024-03-19 00:37:03 +0300

    Finally presentation

---
+++ docs/presentation/sources/emu_compare.py
@@ -0,0 +1,18 @@
+from matplotlib import pyplot as plt
+
+results: dict[str, int] = {"QEMU": 30525, "Kopycat": 4302, "Unicorn": 26751, "MARS": 5648, "SPIM": 23418, "Rush": 31665}
+results = dict(sorted(results.items(), key=lambda item: item[1]))
+plt.rcdefaults()
+plt.bar(results.keys(), results.values(), align="center")
+
+def addlabels(x, y):
+    for i in range(len(x)):
+        plt.text(i, y[i], y[i], ha="center")
+        
+addlabels(results.keys(), list(results.values()))
+plt.xlabel("Emulators")
+plt.xticks(rotation=30)
+plt.ylabel("Performance, IPS")
+plt.title("Results of comparison of performance of MIPS core emulators")
+plt.tight_layout()
+plt.savefig("./emu_compare.jpg", dpi=600)
