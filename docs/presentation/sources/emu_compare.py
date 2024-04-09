67eb0bfac861982eaafe51a60256171768166a18
Kutaev Kirill <kirSM2010@gmail.com>
2024-04-10 01:17:18 +0300

    Updated graphics

--- docs/presentation/sources/emu_compare.py
+++ docs/presentation/sources/emu_compare.py
@@ -1,17 +1,19 @@
 from matplotlib import pyplot as plt
 
-results: dict[str, int] = {"QEMU": 30525, "Kopycat": 4302, "Unicorn": 26751, "MARS": 5648, "SPIM": 23418, "Rush": 0}
+results: dict[str, int] = {"QEMU": 30525, "Kopycat": 5648, "Unicorn": 24751, "MARS": 4302, "SPIM": 15824, "Rush": 25073}
 results = dict(sorted(results.items(), key=lambda item: item[1]))
 plt.rcdefaults()
 plt.bar(results.keys(), results.values(), align="center")
 
 def addlabels(x, y):
     for i in range(len(x)):
-        plt.text(i, y[i], y[i], ha="center")
+        plt.text(i, y[i], y[i], ha="center", fontsize=15)
         
 addlabels(results.keys(), list(results.values()))
-plt.xlabel("Эмуляторы")
-plt.xticks(rotation=30)
-plt.ylabel("Быстродействие, IPS")
+plt.xlabel("Эмуляторы", fontsize=12)
+# plt.title("Выполнение ПО, реализующего алгоритм поиска простых чисел", fontsize=10)
+plt.xticks(fontsize=15)
+plt.yticks(fontsize=15)
+plt.ylabel("Быстродействие, IPS", fontsize=12)
 plt.tight_layout()
 plt.savefig("./emu_compare.jpg", dpi=600)
